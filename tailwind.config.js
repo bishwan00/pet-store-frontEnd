25754b3e27fbf893dfd85e3f432775ac121c2bd2
bishwan00 <bishwan.00606094@gmail.com>
2023-04-11 23:03:42 +0300

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,8 @@ export default {
     },
     extend: {
       backgroundImage: {
-        "top-img": "url('./src/asset/top_nav_s4.png')",
+        "top-img":
+          "url('https://homepages.cae.wisc.edu/~ece533/images/airplane.png')",
       },
       fontFamily: {
         NunitoSans: ["NunitoSans", "sans-serif"],
